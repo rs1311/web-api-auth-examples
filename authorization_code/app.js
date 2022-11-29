0a149373510ca4257b20b4402a1f6f5a98f6758d
Raghav Sriram <93463176+rs1311@users.noreply.github.com>
2022-11-29 14:10:19 +0800

    hi

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -13,9 +13,9 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var client_id = '053bb1b3375543f7b4e886159192161f'; // Your client id
+var client_secret = 'e623fcafcb844c9d8940a320385d4b09'; // Your secret
+var redirect_uri = 'http://localhost:8888'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
